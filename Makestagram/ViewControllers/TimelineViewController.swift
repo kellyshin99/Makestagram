8e683dc4cbcf4b191f0f5ad23d0cc00123ea0d89
Kelly Seongeun Shin <kellysshin99@gmail.com>
2015-06-29 13:51:48 -0700

    Added code to fetch data from Parse and began building Timeline

--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 import Parse
 
 class TimelineViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
 
     var photoTakingHelper: PhotoTakingHelper?
+    var posts: [Post] = []
     
     func takePhoto() {
         //instantiate photo taking class, provide callback for when photo is selected
@@ -29,7 +31,34 @@ class TimelineViewController: UIViewController {
         
         self.tabBarController?.delegate = self
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let followingQuery = PFQuery(className: "Follow")
+        followingQuery.whereKey("fromUser", equalTo:PFUser.currentUser()!)
+        
+        let postsFromFollowedUsers = Post.query()
+        postsFromFollowedUsers!.whereKey("user", matchesKey: "toUser", inQuery: followingQuery)
+        
+        let postsFromThisUser = Post.query()
+        postsFromThisUser!.whereKey("user", equalTo: PFUser.currentUser()!)
+        
+        let query = PFQuery.orQueryWithSubqueries([postsFromFollowedUsers!, postsFromThisUser!])
+        
+        query.includeKey("user")
+        
+        query.orderByAscending("createdAt")
+        
+        query.findObjectsInBackgroundWithBlock {(result: [AnyObject]?, error: NSError?) -> Void in
+            self.posts = result as? [Post] ?? []
+            self.tableView.reloadData()
+        }
+        
+    }
+    
 }
+    
 
 // MARK: Tab Bar Delegate
 
@@ -46,7 +75,18 @@ extension TimelineViewController: UITabBarControllerDelegate {
     
 }
 
-
+extension TimelineViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as! UITableViewCell
+        cell.textLabel!.text = "Post"
+        return cell
+    }
+    
+}
     /*
     // MARK: - Navigation
 
